8282d3f6705b301c65768707a23e088041760a19
shielakri <shiela10@msn.com>
2021-01-29 23:18:15 +0100

    Changed code to run the algorithms seven times and use the median of these times to calculate the speedups.

--- exercises/solutions/exercises02/Exercise1.java
+++ exercises/solutions/exercises02/Exercise1.java
@@ -2,10 +2,6 @@
  * A class that measures the runtimes of the algorithms implemented in the
  * 'FindMax' class.
  *
- * Note: When you measure the algorithms, you should run them at least 7 times,
- * and take the median of these times before comparing the algorithms.
- *
- *
  * To use:
  *
  * Compile:
@@ -14,19 +10,30 @@
  * Run:
  * java Exercise1 <n> (where <n> is the size of the array)
  *
- * 
+ *
  * @author Shiela Kristoffersen
  *
  */
 
+import java.util.Arrays;
+
 class Exercise1 {
+
 
   /**
    * @param args Should recieve an integer 'n' that determines the size of the array.
    */
   public static void main(String[] args) {
 
-    int n, seed = 25;
+
+    /**
+     * 'n' is the size of the array.
+     * 'seed' is the number used for generating a specific random sequence.
+     * 'runs' is the number of times each algorithm is run. Choose an odd number to more easily calculate the median.
+     */
+    int n, seed = 100;
+    int runs = 7;
+
 
     /**
      * Catching excpetions in the case of wrong user input.
@@ -44,56 +51,99 @@ class Exercise1 {
 
     FindMax fm = new FindMax(n, seed);
 
+
+    /**
+     * Storing the runtimes in arrays.
+     */
+    double[] runtimeSeq = new double[runs];
+    double[] runtimePar_11 = new double[runs];
+    double[] runtimePar_12 = new double[runs];
+    double[] runtimePar_21 = new double[runs];
+    double[] runtimePar_22 = new double[runs];
+
     System.out.println("\nTesting FindMax with n = " + n + " and cores = " + fm.cores + "\n");
 
-    System.out.println("Sequential Algorithm");
-    long start = System.nanoTime();
-    fm.findMaxSeq();
-    long end = System.nanoTime();
-    double runtimeSeq = (end - start) / 1000000.0;
-    System.out.println("Max\t:\t" + fm.globalMax);
-    System.out.println("Time\t:\t" + runtimeSeq + " ms");
+    for (int i = 0; i < runs; i++) {
 
+      System.out.println("...Run " + (i + 1) + "...");
+
+      long start = System.nanoTime();
+      fm.findMaxSeq();
+      long end = System.nanoTime();
+      runtimeSeq[i] = (end - start) / 1000000.0;
+
+      start = System.nanoTime();
+      fm.findMaxPar_11();
+      end = System.nanoTime();
+      runtimePar_11[i] = (end - start) / 1000000.0;
+
+
+      start = System.nanoTime();
+      fm.findMaxPar_12();
+      end = System.nanoTime();
+      runtimePar_12[i] = (end - start) / 1000000.0;
+
+      start = System.nanoTime();
+      fm.findMaxPar_21();
+      end = System.nanoTime();
+      runtimePar_21[i] = (end - start) / 1000000.0;
+
+      start = System.nanoTime();
+      fm.findMaxPar_22();
+      end = System.nanoTime();
+      runtimePar_22[i] = (end - start) / 1000000.0;
+    }
+
+
+    /**
+     * Sorting the arrays in ascending order.
+     */
+    Arrays.sort(runtimeSeq);
+    Arrays.sort(runtimePar_11);
+    Arrays.sort(runtimePar_12);
+    Arrays.sort(runtimePar_21);
+    Arrays.sort(runtimePar_22);
+
+
+    /**
+     * Finding medians. Here I'm assuming that 'runs' is an odd number.
+     */
+    double medianSeq = runtimeSeq[runs / 2];
+    double medianPar_11 = runtimePar_11[runs / 2];
+    double medianPar_12 = runtimePar_12[runs / 2];
+    double medianPar_21 = runtimePar_21[runs / 2];
+    double medianPar_22 = runtimePar_22[runs / 2];
+
+
+    /**
+     * Printing out the maximum number found, the runtime and the speedup
+     * for each algorithm.
+     */
+    System.out.println("\nSequential Algorithm");
+    System.out.println("Max\t:\t" + fm.globalMax);
+    System.out.println("Time\t:\t" + medianSeq + " ms");
 
     System.out.println("\nParallel Algorithm 1.1");
-    start = System.nanoTime();
-    fm.findMaxPar_11();
-    end = System.nanoTime();
-    printInfo(fm.globalMax, start, end, runtimeSeq);
-
+    printInfo(fm.globalMax, medianPar_11, medianSeq);
 
     System.out.println("\nParallel Algorithm 1.2");
-    start = System.nanoTime();
-    fm.findMaxPar_12();
-    end = System.nanoTime();
-    printInfo(fm.globalMax, start, end, runtimeSeq);
-
+    printInfo(fm.globalMax, medianPar_12, medianSeq);
 
     System.out.println("\nParallel Algorithm 2.1");
-    start = System.nanoTime();
-    fm.findMaxPar_21();
-    end = System.nanoTime();
-    printInfo(fm.globalMax, start, end, runtimeSeq);
-
+    printInfo(fm.globalMax, medianPar_21, medianSeq);
 
     System.out.println("\nParallel Algorithm 2.2");
-    start = System.nanoTime();
-    fm.findMaxPar_22();
-    end = System.nanoTime();
-    printInfo(fm.globalMax, start, end, runtimeSeq);
-
+    printInfo(fm.globalMax, medianPar_22, medianSeq);
   }
 
+
   /**
-   * Calculates runtime and speedup, and prints out information to the terminal.
+   * Calculates speedup, and prints out information to the terminal.
    * @param max        The maximum integer found.
-   * @param start      The start time of the algorithm.
-   * @param end        The end time of the algorithm.
+   * @param runtimePar The runtime of the parallel algorithm.
    * @param runtimeSeq The runtime of the sequential algorithm.
    */
-  static void printInfo(int max, long start, long end, double runtimeSeq) {
-
-    double runtimePar = (end - start) / 1000000.0;
+  static void printInfo(int max, double runtimePar, double runtimeSeq) {
     System.out.println("Max\t:\t" + max);
     System.out.println("Time\t:\t" + runtimePar + " ms");
     System.out.println("Speedup\t:\t" + runtimeSeq / runtimePar);
